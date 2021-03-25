acb37b5f3df523da6f1c8e28698e9b1f1e201f7c
anacesar <pereira_ana99@hotmail.com>
2021-03-25 18:48:27 +0000

    comiti

--- TP1/src/atletas.py
+++ TP1/src/atletas.py
@@ -2,18 +2,37 @@
 imprimir o nome (convertido para maiúsculas) de todos os concorrentes que se inscrevem como
 'Individuais' e são de 'Valongo'.
 '''
-
-
 import re
 
 atletas = []
 
-atleta = re.compile(r'"nome":"(([a-zA-Z ])+")')
+name_regex = re.compile(r'"nome":"([a-zA-Z ]*)"')
+birth_regex = re.compile(r'"dataNasc":"(.+)"')
 
-f = open("teste.json")
 
+def stuff(info): 
+    name = name_regex.search(info).group(1)
+    ##birth = birth_regex.search()
+
+def func(conteudo): 
+    
+    cenas = re.findall(r'{[^{]+}', conteudo)
+    for m in cenas: 
+        stuff(m)
+        #if(cenas) : print(cenas.group())
+    
+
+with open("inscritos-form.json") as f: 
+    conteudo = f.read()
+    func(conteudo)
+
+
+'''
 for linha in f:
     res = atleta.findall(linha)
     if(res):
         print(res)
+'''
+
+
 
